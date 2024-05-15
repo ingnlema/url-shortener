ed203ed839640b7630cd22c6005cfb249196bb64
ingnlema <ing.nlema@gmail.com>
2024-05-15 18:52:32 -0300

    hot fix test cases

--- src/test/java/com/mercadolibre/urlshortener/exception/UrlAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.mercadolibre.urlshortener.exception;
-
-public class UrlAlreadyExistsException extends RuntimeException {
-    public UrlAlreadyExistsException(String message) {
-        super(message);
-    }
-}
